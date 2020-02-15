9a98df3689ac43fc6edfaf048e9d61340b3b617a
Jackson Stark <jacksonstark77@hotmail.com>
2020-02-15 14:52:42 -0800

    implement todo list skeleton and style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 50%;
+}
+nav {
+  padding: 20px 20px 20px 0;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <form @submit.prevent="addTodo">
+        <input type="text" placeholder="Enter a todo..." v-model="todo">
+      </form>
+
+      {{ todo }}
+      <ul>
+        <li v-for="(data, index) in todos" :key="index">{{ data.todo }}</li>
+      </ul>
+      <h3>These are your Todos for the day</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  data() {
+    return {
+      todo: "",
+      todos: [{ todo: "Go to movie" }, { todo: "Do laundry" }]
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ todo: this.todo });
+      this.todo = "";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.holder {
+  background: #fff;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 10px solid #3e9df6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+</style>
